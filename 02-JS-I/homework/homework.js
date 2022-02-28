7e029d8773ceb4256aaca2fe9c429ce2d6cf8a08
Bruno <brookwine@gmail.com>
2022-02-28 12:41:52 -0300

    Commit Final

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -71,11 +71,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  var var1 = "gato";
-  var var2 = "perro";
-  var str1 = var1.lenght;
-  var str2 = var2.lenght;
-  if(str1===str2){
+  if(str1.length===str2.length){
     return true;
   }
   return false;
@@ -170,11 +166,9 @@ function esPositivo(numero) {
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
   if(numero > 0){
-    var1 = "Es Postivo";
-    return var1;
+    return "Es positivo"
   }else if(numero < 0){
-    var2 = "Es negativo"
-    return var2;
+    return "Es negativo"
   }else{
     return false;
   }
@@ -184,7 +178,7 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  return srt + "!";
+  return str + "!"
 }
 
 function combinarNombres(nombre, apellido) {
@@ -198,7 +192,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return "Hola" + " " + nombre;
+  return "Hola " + nombre + "!";
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
@@ -236,7 +230,13 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if (letra.length > 1){
+    return "Dato incorrecto"
+  } else if(letra === "a" ||  letra === "e" || letra === "i" || letra === "o" || letra ==="u") {
+    return "Es vocal"
+    } else{
+      return "Dato incorrecto"
+    }
 }
 
 
